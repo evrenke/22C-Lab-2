f10c4c91efdf6f06c9ab57eff836af34e3e9039c
evrenke <evrenkeskin300@gmail.com>
2018-10-15 21:53:02 -0700

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "List.h"
+
+template <class ObjectType>
+class Stack : protected List
+{
+public:
+	void push(ObjectType ot);
+	void pop(ObjectType ot);
+	bool isEmpty();
+};
